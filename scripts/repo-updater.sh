e4454ecac4c73e69b9faa73b63b82c5c538b9166
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2023-11-13 16:26:51 +0000

    Update repo-updater.sh

--- scripts/repo-updater.sh
+++ scripts/repo-updater.sh
@@ -1,5 +1,5 @@
 #need to manually map *.tcz.* files to skip *.tcz.zsync and md5
-rsync -av --delete --include="*/*/tcz/*.tcz.dep" --include="*/*/tcz/*.tcz.list" --include="*/*/tcz/*.tcz.info" --include="*/*/tcz/*.tcz.tree" --include="*/" --exclude="*" repo.tinycorelinux.net::tc ./tinycorelinux
+rsync -av --delete --prune-empty-dirs --include="*/*/tcz/*.tcz.dep" --include="*/*/tcz/*.tcz.list" --include="*/*/tcz/*.tcz.info" --include="*/*/tcz/*.tcz.tree" --include="*/" --exclude="*" repo.tinycorelinux.net::tc ./tinycorelinux
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'actions@github.com'
 git add .
