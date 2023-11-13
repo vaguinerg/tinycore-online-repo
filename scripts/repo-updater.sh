08659db0d15e4cfa87f467a59d4938e775d6726c
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2023-11-13 15:59:59 +0000

    Update repo-updater.sh

--- scripts/repo-updater.sh
+++ scripts/repo-updater.sh
@@ -1,4 +1,4 @@
-rsync -av --delete repo.tinycorelinux.net::tc ./tinycorelinux
+rsync -av --delete --include="*/*/tcz/*.tcz.*" --include="*/" --exclude="*" repo.tinycorelinux.net::tc ./tinycorelinux
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'actions@github.com'
 git add .
