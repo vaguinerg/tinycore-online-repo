5adb57b7e37c752011932616a207a71eee0c2ff5
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2023-11-13 13:58:41 +0000

    Update repo-updater.sh

--- scripts/repo-updater.sh
+++ scripts/repo-updater.sh
@@ -1,6 +1,6 @@
-echo tst >> xd
+rsync -av --delete repo.tinycorelinux.net::tc ./tinycorelinux
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'actions@github.com'
 git add .
-git commit -m "Update ISO file"
+git commit -m "Update"
 git push
