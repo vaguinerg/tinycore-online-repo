4c2e48c99fb69df8a31456936de9c735741d1210
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2023-11-13 13:57:37 +0000

    Update repo-updater.sh

--- scripts/repo-updater.sh
+++ scripts/repo-updater.sh
@@ -1,1 +1,6 @@
-echo ${GITHUB_WORKSPACE}
+echo tst >> xd
+git config --global user.name 'GitHub Actions'
+git config --global user.email 'actions@github.com'
+git add .
+git commit -m "Update ISO file"
+git push
