c5416b16c110f6642318ea59fcbf61b5e286b50d
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2023-11-16 12:11:45 +0000

    Update repo-updater.sh

--- scripts/repo-updater.sh
+++ scripts/repo-updater.sh
@@ -4,42 +4,41 @@ sudo apt-get install -y jq
 #need to manually map *.tcz.* files to skip *.tcz.zsync and md5
 rsync -av --size-only --delete --prune-empty-dirs --include="*/*/tcz/*.tcz.dep" --include="*/*/tcz/*.tcz.list" --include="*/*/tcz/*.tcz.info" --include="*/*/tcz/*.tcz.tree" --include="*/" --exclude="*" repo.tinycorelinux.net::tc ./tinycorelinux
 
-for version_dir in tinycorelinux/*.x; do
-	version=$(basename "$version_dir")
-	for arch_dir in tinycorelinux/$version/*; do
-		arch=$(basename "$arch_dir")
-		mkdir -p data/$version/$arch/
-  		output_directory="data/$version/$arch"
-		find tinycorelinux/$version/$arch/tcz/*.list -maxdepth 1 -type f -exec basename {} \; | sed 's/\.list//' > "$output_directory/tczlist"
-		
-  		#I couldn't get JQ to work, so I'm going to use this aberration that is working.
-		echo "{" > "$output_directory/provides"
-		for file in tinycorelinux/$version/$arch/tcz/*.list; do
-		    name=$(basename "$file" .list)
+mapfile -t tcvers < <(find tinycorelinux -mindepth 2 -maxdepth 2 -type d -exec sh -c 'echo "$(basename $(dirname {})) $(basename {})"' \;)
+
+for item in "${tcvers[@]}"; do
+  version=$(echo "$tcvers" | awk '{print $1}')
+  arch=$(echo "$tcvers" | awk '{print $2}')
+  [ -d "data/$version/$arch" ] || mkdir -p "data/$version/$arch"
+  output_directory="data/$version/$arch"
+  input_directory="tinycorelinux/$version/$arch/tcz"
+  
+  find $input_directory/*.list -maxdepth 1 -type f -exec basename {} \; | sed 's/\.list//' > "$output_directory/tczlist"
+  
+  for type in provides sizelist taglist; do
+	echo "{" > "$output_directory/$type"
+	for file in $input_directory/*.list; do
+             name=$(basename "$file" .list)
+             case $type in
+                provides)
 		    echo "\"$name\": [" >> "$output_directory/provides"
 		    while IFS= read -r line || [ -n "$line" ]; do
 		        echo "\"$line\"," >> "$output_directory/provides"
-		    done < "$file"
+		    done < "${name}.list"
 		    echo "]," >> "$output_directory/provides"
-		done
-		echo "}" >> "$output_directory/provides"
-
-		echo "{" > "$output_directory/sizelist"
-		for file in tinycorelinux/$version/$arch/tcz/*.info; do
-		    name=$(basename "$file" .info)
-		    size=$(grep -Eo 'Size:[[:space:]]+[0-9.]+[[:alpha:]]+' "$file" | awk '{print $2}')
+                    ;;
+                sizelist)
+		    size=$(grep -Eo 'Size:[[:space:]]+[0-9.]+[[:alpha:]]+' "${name}.info" | awk '{print $2}')
 		    echo "\"$name\": \"$size\"," >> "$output_directory/sizelist"
-		done
-		echo "}" >> "$output_directory/sizelist"
-		
-		echo "{" > "$output_directory/taglist"
-		for file in tinycorelinux/$version/$arch/tcz/*.info; do
-		    name=$(basename "$file" .info)
-		    tags=$(grep -Eo 'Tags:[[:space:]]+.+' "$file" | awk '{$1=""; print $0}' | tr -s ' ' | sed 's/^[[:space:]]//')
+                    ;;
+                taglist)
+		    tags=$(grep -Eo 'Tags:[[:space:]]+.+' "${name}.info" | awk '{$1=""; print $0}' | tr -s ' ' | sed 's/^[[:space:]]//')
 		    echo "\"$name\": [\"$tags\"]," >> "$output_directory/taglist"
-		done
-		echo "}" >> "$output_directory/taglist"
- 	done
+                    ;;
+            esac
+	done
+ 	echo "}" >> "$output_directory/$type"
+  done
 done
 
 echo "{" > "site-data/versions"
